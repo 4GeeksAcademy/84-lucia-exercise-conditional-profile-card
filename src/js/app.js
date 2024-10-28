a27f59df773b54d66bad288086527ce75b33e736
luciacanalda <luciacanaldas@gmail.com>
2024-10-28 19:49:01 +0000

    Cambio social

--- src/js/app.js
+++ src/js/app.js
@@ -39,7 +39,11 @@ function render(variables = {}) {
           ${variables.city == null ? "Miami" : variables.city}
           ${variables.country == null ? "USA" : variables.country}
           </h3>
-          <ul class="position-right">
+          <ul class="${
+            variables.socialMediaPosition == null
+              ? "right"
+              : variables.socialMediaPosition
+          }>
             <li><a href="https://twitter.com/${
               variables.twitter == null ? "4Geeks" : variables.twitter
             }"><i class="fab fa-twitter"></i></a></li>
