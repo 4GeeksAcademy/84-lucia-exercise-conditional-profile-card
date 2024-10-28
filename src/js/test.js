9d1f69d4b0bdc7c007218f9428adf18e2cc04902
luciacanalda <luciacanaldas@gmail.com>
2024-10-28 18:41:47 +0000

    Commit

---
+++ src/js/test.js
@@ -0,0 +1,15 @@
+let name = "Lucy"
+let edad = "10"
+
+
+let phrase = `El nombre es ${name}; y mi edad es $ {edad} años`;
+
+$´{variables.name 00 null ? "Lucy" : variables.name}
+
+let names = ["Celia", "Denes", "Diego"];
+let resultMap = names.map ((value, index, array)=>{
+
+});
+
+
+console.log (resultMap);
